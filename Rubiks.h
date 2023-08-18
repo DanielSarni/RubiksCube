f6a620b98476e1832828fdedc2551b8699638f75
Vergonyah <vergonyah@vergonyah.lan1>
2023-08-18 15:34:57 -0700

    making class

---
+++ Rubiks.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class RubiksCube {
+    public:
+        void rotateFrontClockwise();
+        void rotateBackClockwise();
+        void rotateLeftClockwise();
+        void rotateRightClockwise();
+        void rotateTopClockwise();
+        void rotateBottomClockwise();
+        void showCube();
+
+        
+}
